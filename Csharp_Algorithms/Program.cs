72d02444de9552c57d9454d6ccac92ffb6a2b63b
FaridNesirlii <tu7hnl5wt@code.edu.az>
2022-11-16 17:16:42 +0400

    new

--- Csharp_Algorithms/Program.cs
+++ Csharp_Algorithms/Program.cs
@@ -11,17 +11,18 @@ namespace Csharp_Algorithms
     {
         static void Main(string[] args)
         {
+
             /*-------------------1--------------------------
-             int numm = Convert.ToInt32(Console.ReadLine());
-            for (int num = Convert.ToInt32(Console.ReadLine()); num < numm; num++)
+         int numm = Convert.ToInt32(Console.ReadLine());
+        for (int num = Convert.ToInt32(Console.ReadLine()); num < numm; num++)
+        {
+            if (num % 2 != 0)
             {
-                if (num % 2 != 0)
-                {
-                    Console.WriteLine(num);
-                }
+                Console.WriteLine(num);
             }
-                        
-            -------------------------------------------------*/
+        }
+
+        //------------------------------------------------- */
 
 
 
@@ -31,7 +32,8 @@ namespace Csharp_Algorithms
 
 
 
-            /*------------------2--------------------------
+            ///------------------2--------------------------]
+            /*
             int numm = 500;
             
             for (int num = 5; num < numm; num++)
@@ -44,8 +46,8 @@ namespace Csharp_Algorithms
                     }
                 }
             }
-            -------------------------------------------------*/
-
+           // -------------------------------------------------/
+            */
 
 
 
@@ -62,113 +64,54 @@ namespace Csharp_Algorithms
 
 
             string ad = "azerbaycan";
-           char a = 'a';
-           char b = 'b';
-           char c = 'c';
-           char e = 'e';
-           char r='r';
-           char n = 'n';
-           char z = 'z';
-           int count = 0;
-           int count1 = 0;
-           int count2 = 0;
-           int count3 = 0;
-           int count4 = 0;
-           int count5 = 0;
-           int count6 = 0;
-
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (a == ad[i])
-               {
-                   count++;
-               }
-           }
-           Console.WriteLine($"a:{count}");
-
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (b == ad[i])
-               {
-                   count1++;
-               }
-           }
-           Console.WriteLine($"b:{count1}");
-
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (c == ad[i])
-               {
-                   count2++;
-               }
-           }
-           Console.WriteLine($"c:{count2}");
-
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (e == ad[i])
-               {
-                   count3++;
-               }
-           }
-           Console.WriteLine($"e:{ count3}");
-
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (r == ad[i])
-               {
-                   count4++;
-               }
-           }
-           Console.WriteLine($"r:{count4}");
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (n == ad[i])
-               {
-                   count5++;
-               }
-           }
-           Console.WriteLine($"n:{count5}");
-
-           for (int i = 0; i < ad.Length; i++)
-           {
-               if (z == ad[i])
-               {
-                   count6++;
-               }
-           }
-           Console.WriteLine($"z:{count6}");
-
-
-
-           
-
-
-
-
-
-
-            /*
-             
-----------* BUNU INTERNETDEN TAPDIM AMMA BIZ BUNU KECMEMISHIQ YEQIN*-----------------
-             
-            string str = "azerbaycan";
-            while (str.Length > 0)
+            char[] chars = ad.ToCharArray();
+            for (int i = 0; i < ad.Length; i++)
             {
-                Console.Write(str[0] + " = ");
-                int cal = 0;
-                for (int i = 0; i < str.Length; i++)
+                int count = 0;
+                for (int j = 0; j < chars.Length; j++)
                 {
-                    if (str[0] == str[i])
+                    if (chars[j] == ad[i])
                     {
-                        cal++;
+                        chars[j] = ' ';
+                        count++;    
                     }
+
                 }
-                Console.WriteLine(cal);
-                str = str.Replace(str[0].ToString(), string.Empty);
+                if (count != 0)
+                {
+                    Console.WriteLine($"{chars[i]}-{count}");
+                }
             }
-        */
-        
+
+
+
+
+
+
+
+
+
+
+
+            //----------* BUNU INTERNETDEN TAPDIM AMMA BIZ BUNU KECMEMISHIQ YEQIN*-----------------
+
+            //string str = "azerbaycan";
+            //while (str.Length > 0)
+            //{
+            //    Console.Write(str[0] + " = ");
+            //    int cal = 0;
+            //    for (int i = 0; i < str.Length; i++)
+            //    {
+            //        if (str[0] == str[i])
+            //        {
+            //            cal++;
+            //        }
+            //    }
+            //    Console.WriteLine(cal);
+            //    str = str.Replace(str[0].ToString(), string.Empty);
+            //}
+            // */
+
 
         }
     }
